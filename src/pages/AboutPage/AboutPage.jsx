4434b44fd756e6af45d5f69fd4385bbe989f2ba9
Morozov Ivan <morosow.iwan@gmail.com>
2023-06-29 23:24:37 +0200

    photo compressed, LazyLoadImage added

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./AboutPage.module.css";
+// import s from "./AboutPage.module.css";
 
 export default function AboutPage() {
   return <div>AboutPage</div>;
